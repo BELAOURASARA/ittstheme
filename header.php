344e8376fd465f05d0d31e442a4d29aa1a48dc1e
BELAOURASARA <hs_belaoura@esi.dz>
2020-12-13 21:59:34 +0100

    adding static dark header-top

--- header.php
+++ header.php
@@ -36,6 +36,9 @@
 <body>
 
     <header >
+            <div class="header-top">
+                
+            </div>
             <div class="logo">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png">
             </div>
